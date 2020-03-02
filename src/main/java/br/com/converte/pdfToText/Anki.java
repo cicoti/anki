015b103252a2fcd727d5ec5e48222edfca06f986
silvio cicoti <silvio.cicoti@comprealugueagora.com.br>
2020-03-02 14:01:24 -0300

    primeiro commit

---
+++ src/main/java/br/com/converte/pdfToText/Anki.java
@@ -0,0 +1,41 @@
+package br.com.converte.pdfToText;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Anki {
+	
+	public ArrayList<String> blocos;
+	public String textoIngles;
+	public String textoPortugues;
+	public File arquivoAudioIngles;
+	
+	public ArrayList<String> getBlocos() {
+		return blocos;
+	}
+	public void setBlocos(ArrayList<String> blocos) {
+		this.blocos = blocos;
+	}
+	public String getTextoIngles() {
+		return textoIngles;
+	}
+	public void setTextoIngles(String textoIngles) {
+		this.textoIngles = textoIngles;
+	}
+	public String getTextoPortugues() {
+		return textoPortugues;
+	}
+	public void setTextoPortugues(String textoPortugues) {
+		this.textoPortugues = textoPortugues;
+	}
+	public File getArquivoAudioIngles() {
+		return arquivoAudioIngles;
+	}
+	public void setArquivoAudioIngles(File arquivoAudioIngles) {
+		this.arquivoAudioIngles = arquivoAudioIngles;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/br/com/converte/pdfToText/CustomPDFTextStripper.java
@@ -0,0 +1,20 @@
+package br.com.converte.pdfToText;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.TextPosition;
+
+class CustomPDFTextStripper extends PDFTextStripper
+	{
+	    //Vector<Vector<List<TextPosition>>> data = new Vector<Vector<List<TextPosition>>>();
+	    public CustomPDFTextStripper() throws IOException {
+	        super();
+	    }
+
+	    public List<List<TextPosition>> getCharactersByArticle(){
+	       // data.add(charactersByArticle);
+	        return charactersByArticle;
+	    }
+	}
