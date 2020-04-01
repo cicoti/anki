778f12a37930a72f9f0cf325af7f57b1bbfbb3a6
silvio cicoti <silvio.cicoti@comprealugueagora.com.br>
2020-04-01 19:13:16 -0300

    complemento

--- src/main/java/br/com/anki/AnkiMineracao.java
+++ src/main/java/br/com/anki/AnkiMineracao.java
@@ -21,65 +21,65 @@ public class AnkiMineracao {
 	DesktopOptions options;
 	WiniumDriverService service;
 	WiniumDriver driver;
-	
+
 	/*
 	 * public static void main(String args[]) throws InterruptedException,
 	 * IOException, AWTException {
-	 * 
-	 * AnkiAuto ankiAuto = new AnkiAuto(); 
-	 * ankiAuto.setUp(); 
+	 *
+	 * AnkiAuto ankiAuto = new AnkiAuto();
+	 * ankiAuto.setUp();
 	 * ankiAuto.baralho();
 	 * ankiAuto.tearDown();
-	 * 
+	 *
 	 * System.out.println("FIM!");
-	 * 
+	 *
 	 * }
 	 */
-    
+
 	public AnkiMineracao(int baralho) throws InterruptedException, IOException, AWTException {
 		this.baralho = baralho;
 		this.setUp();
 		this.baralho();
-				
+
 	}
-	
+
     public void setUp() throws IOException, InterruptedException{
-		
+
     	this.tearDown();
-    	
+
     	options = new DesktopOptions();
 		options.setApplicationPath("C:\\Program Files (x86)\\Anki\\anki.exe");
-		
-		File driverPath = new File("C:\\Projetos\\automatizaranki\\Winium.Desktop.Driver.exe");
+
+		File driverPath = new File("C:\\winium\\Winium.Desktop.Driver.exe");
 		//File driverPath = new File("..\\Winium.Desktop.Driver.exe");
 		service = new WiniumDriverService.Builder().usingDriverExecutable(driverPath).usingPort(9999).withVerbose(true).withSilent(true).buildDesktopService();
-		
+
 		try {
 			service.start();
 		} catch (IOException e) {
 			System.out.println("Exception while starting WINIUM service");
 			e.printStackTrace();
 		}
-		
-		
+
+
 		driver = new WiniumDriver(service,options);
-		
+
 		 try {
-			 
+
 			 robot = new Robot();
-			 
+
 		  } catch (Exception failed) {
 		   System.err.println("Failed instantiating Robot: " + failed);
-		  }  
-		 
-		 
-		 
+		  }
+
+
+
 	}
-    
+
     protected void baralho() throws InterruptedException, IOException, AWTException {
     	WiniumDriver driver = new WiniumDriver(new URL("http://127.0.0.1:9999"), options);
         Thread.sleep(10000);
-        
+
         if(baralho == 1) {
         // baralho master
     	robot.keyPress(KeyEvent.VK_TAB);
@@ -87,14 +87,14 @@ public class AnkiMineracao {
         //robot.keyPress(KeyEvent.VK_TAB);
         //Thread.sleep(2000);
         }
-        
+
         if(baralho == 2) {
         // baralho v2 // Testes
         robot.keyPress(KeyEvent.VK_TAB);
         Thread.sleep(2000);
         robot.keyPress(KeyEvent.VK_TAB);
         }
-        
+
         if(baralho == 3) {
             // baralho estudo palavras
             robot.keyPress(KeyEvent.VK_TAB);
@@ -103,95 +103,103 @@ public class AnkiMineracao {
             Thread.sleep(2000);
             robot.keyPress(KeyEvent.VK_TAB);
         }
-                       
+
         Thread.sleep(1000);
-        
+
         // Executar baralho
         robot.keyPress(KeyEvent.VK_ENTER);
         Thread.sleep(1000);
         robot.keyPress(KeyEvent.VK_A);
 
     }
-    
+
 	public void fraseIngles(String fraseIngles) throws InterruptedException {
-		 
+
 		//robot.keyPress(KeyEvent.VK_TAB);
         Thread.sleep(2000);
 		 copiarColar(fraseIngles);
-	
+
 	}
-	
+
+	public void audioIngles(File caminhoArquivo) throws InterruptedException {
+
+		 Thread.sleep(2000);
+		 robot.keyPress(KeyEvent.VK_F3);
+		 driver.findElementByName("Nome").click();
+		 copiarColar(caminhoArquivo.getAbsolutePath());
+		 robot.keyPress(KeyEvent.VK_ENTER);
+
+	}
+
 	public void audioIngles() throws InterruptedException {
-		 
+
 		Thread.sleep(1000);
 		robot.keyPress(KeyEvent.VK_CONTROL);
 		robot.keyPress(KeyEvent.VK_T);
 		Thread.sleep(1000);
 		driver.findElementByName("Record").click();
-		//copiarColar(fraseIngles);
-		//robot.keyPress(KeyEvent.VK_ENTER);
 		Thread.sleep(4000);
 		robot.keyRelease(KeyEvent.VK_Z);
 		robot.keyRelease(KeyEvent.VK_CONTROL);
-	     
+
 	}
-		
+
 	public void frasePortugues(String frasePortugues) throws InterruptedException {
-				
+
 		Thread.sleep(2000);
 		robot.keyPress(KeyEvent.VK_TAB);
 		copiarColar(frasePortugues);
 	    adicionarCartao();
-		
+
 	}
-	
+
 	protected void adicionarCartao() throws InterruptedException {
-		
-		Thread.sleep(2000); 
+
+		Thread.sleep(2000);
 		robot.keyPress(KeyEvent.VK_CONTROL);
 		robot.keyPress(KeyEvent.VK_ENTER);
 		Thread.sleep(1000);
 		robot.keyRelease(KeyEvent.VK_CONTROL);
 
-	
+
 	}
-	
+
 	public void fecharAplicacao() throws InterruptedException, IOException {
-		
+
 		Thread.sleep(1000);
 		driver.findElementByName("Fechar").click();
 		Thread.sleep(1000);
 		driver.findElementById("Close").click();
-		
+
 		this.tearDown();
-				
+
 	}
-	
+
 	public void copiarColar(String frase) {
-		 
+
 		 StringSelection stringSelection = new StringSelection(frase);
 		 Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 		 clipboard.setContents(stringSelection, stringSelection);
-		 
+
 		 robot.delay(1000);
-		 robot.keyPress(KeyEvent.VK_CONTROL); 
+		 robot.keyPress(KeyEvent.VK_CONTROL);
 		 robot.keyPress(KeyEvent.VK_V);
 		 robot.delay(1000);
 		 robot.keyRelease(KeyEvent.VK_CONTROL);
 		 robot.keyRelease(KeyEvent.VK_V);
-		
-		
+
+
 	}
 
     public void tearDown() throws IOException, InterruptedException {
-    	
+
     	Process process = Runtime.getRuntime().exec("TASKKILL /F /IM Winium.Desktop.Driver.exe");
 		process.waitFor();
 		if(process.isAlive()) {
 			process.destroy();
 		}
-    	
+
     }
-    
+
 
 }
---
+++ src/main/java/br/com/anki/AnkiTXTMainV2.java
@@ -0,0 +1,85 @@
+package br.com.anki;
+
+import java.awt.AWTException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnkiTXTMainV2 {
+
+	public static void main(String[] args) throws IOException, InterruptedException, AWTException {
+
+		boolean isTeste = false;
+
+		int recomecar = 10;
+
+		String fileTXTName = "Lost and Found.txt";
+		String path = "\\src\\resource\\CursoInglesMairoVergara\\10 - Lost and Found\\part 8\\";
+
+		File file = new File(new File(".").getCanonicalPath().concat(path.concat(fileTXTName)));
+		Path pathFileTXT = Paths.get(file.getAbsolutePath());
+		byte[] bytes = Files.readAllBytes(pathFileTXT);
+		List<String> allLines = Files.readAllLines(pathFileTXT, StandardCharsets.UTF_8);
+		int passo = 3;
+		List<Anki> listAnki = new ArrayList<Anki>();
+		for(int i = 0; i<(allLines.size()/passo); i++) {
+			File fileMp3 = new File(new File(".").getCanonicalPath().concat(path.concat("".concat(new Integer(i+1).toString()).concat(".mp3"))));
+			Anki anki = new Anki();
+			if(fileMp3.exists()) anki.setArquivoAudioIngles(fileMp3);
+			anki.setTextoIngles(allLines.get(1+(i*passo)));
+			anki.setTextoPortugues(allLines.get(2+(i*passo)));
+			listAnki.add(anki);
+		}
+
+		if (!isTeste==true) {
+
+
+			AnkiMineracao ankiMineracao = new AnkiMineracao(1);
+
+			 for(Anki anki : listAnki) {
+
+				 int pos = listAnki.indexOf(anki);
+				 System.out.println(pos);
+				 if(pos>=recomecar) {
+					 System.out.println("Texto Ingles:" + anki.getTextoIngles());
+					 System.out.println("Audio Ingles:" + anki.getArquivoAudioIngles());
+					 System.out.println("Texto Portugues:" + anki.getTextoPortugues());
+					 System.out.println("--------");
+
+					 ankiMineracao.fraseIngles(anki.getTextoIngles().concat(" "));
+					 if(anki.getArquivoAudioIngles()!=null)  ankiMineracao.audioIngles(anki.getArquivoAudioIngles());
+					 else ankiMineracao.audioIngles();
+					 ankiMineracao.frasePortugues(anki.getTextoPortugues().concat(" "));
+				 }
+			 }
+
+			 ankiMineracao.fecharAplicacao();
+
+		 }	else {
+
+			 for(Anki anki : listAnki) {
+
+				 int pos = listAnki.indexOf(anki);
+				 System.out.println(pos);
+				 if(pos>=recomecar) {
+					 System.out.println("Texto Ingles:" + anki.getTextoIngles());
+					 System.out.println("Audio Ingles:" + anki.getArquivoAudioIngles());
+					 System.out.println("Texto Portugues:" + anki.getTextoPortugues());
+					 System.out.println("--------");
+				 }
+			 }
+
+		 }
+
+		 System.out.println("FIM!");
+
+	}
+
+
+
+}
